eaaaf3402a2b24e42562b32a5503145d8f88bb88
NIGGA <valle.nicola.studente@itispaleocapa.it>
2025-01-10 11:28:13 +0100

    Updated as Mr. Grena wanted it to run

--- src/main/java/com/example/progettoinvernale/Stadio.java
+++ src/main/java/com/example/progettoinvernale/Stadio.java
@@ -7,14 +7,16 @@ public class Stadio extends Thread{
     public static PostiASedere[] posti = new PostiASedere[41507];
     public boolean cassa;
     public String name;
-    public String nomeCassa;
     public static int bigliettiDisponibili;
     public int tempNum;
+    public int coda;
+    public int capacitaStadium=41507;
     public static int postiOccupati=0;
-    public void set(boolean c, String n, String nC){
+    public void set(boolean c, String n){
+        Random r = new Random();
         cassa=c;
         name=n;
-        nomeCassa=nC;
+        coda=r.nextInt(2)+1;
         for(int i=0;i<biglietti.capacitaStadium;i++){
             posti[i] = new PostiASedere(biglietti.getBiglietto(i).codice);
         }
@@ -24,46 +26,58 @@ public class Stadio extends Thread{
         Stadio ingressoSud = new Stadio();
         Stadio ingressoEst = new Stadio();
         Stadio ingressoOvest = new Stadio();
-        ingressoNord.set(true, "nord", "cassa nord");
-        ingressoSud.set(true, "sud", "cassa sud");
-        ingressoOvest.set(false, "ovest", "");
-        ingressoEst.set(false, "est", "");
+        Stadio cassaNord = new Stadio();
+        Stadio cassaSud = new Stadio();
+        ingressoNord.set(false, "ingresso nord");
+        ingressoSud.set(false, "ingresso sud");
+        ingressoOvest.set(false, "ingresso ovest" );
+        ingressoEst.set(false, "ingresso est" );
+        cassaSud.set(true,"cassa sud");
+        cassaNord.set(true,"cassa nord");
         ingressoNord.start();
         ingressoSud.start();
         ingressoEst.start();
         ingressoOvest.start();
+        cassaSud.start();
+        cassaNord.start();
     }
     public void run(){
         do {
             if (cassa)
                 compraBiglietto();
+            if(!cassa)
                 entraNelloStadio();
-        }while(biglietti.bigliettiComprati!=biglietti.capacitaStadium);
+        }while(biglietti.bigliettiComprati!=capacitaStadium);
     }
     public void entraNelloStadio(){
         try{
-            TimeUnit.SECONDS.sleep((long) r.nextDouble(1));
+            TimeUnit.MILLISECONDS.sleep(r.nextInt(1000));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         synchronized (posti){
             tempNum=r.nextInt(biglietti.bigliettiComprati);
             if(!posti[tempNum].occupato){
-                System.out.println("Un tifoso si è appena seduto al suo posto");
+                System.out.println("Un tifoso è appena entrato dall'ingresso "+name+" con biglietto con codice: "+posti[tempNum].codicePosto);
                 postiOccupati++;
             }
             posti[tempNum].setOccupato(true);
         }
+        incrementaCoda(r);
     }
     public void compraBiglietto(){
+        incrementaCoda(r);
         try{
-            TimeUnit.SECONDS.sleep(r.nextInt(3));
+            TimeUnit.SECONDS.sleep(r.nextInt(5));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-        biglietti.compraBiglietto();
-        System.out.println("Un tifoso ha appena acquistato un biglietto alla "+nomeCassa);
+        System.out.println("Un tifoso ha appena acquistato un biglietto alla "+name+" con codice "+biglietti.compraBiglietto());
+        coda--;
         System.out.println("I biglietti disponibili per l'acquisto sono: "+biglietti.getBigliettiDisponibili());
+        System.out.println("La coda attuale alla "+name+" è di "+coda+" persone");
+    }
+    public void incrementaCoda(Random r){
+        coda=coda+r.nextInt(3)+1;
     }
 }
